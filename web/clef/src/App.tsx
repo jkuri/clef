6de903b0ae36a2bb2596fbe378115364e7bf534a
Jan Kuri <jkuri88@gmail.com>
2025-07-20 12:00:15 +0200

    chore(web): init frontend project

---
+++ web/clef/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-svh items-center justify-center">
+      <h1 className="text-center font-bold text-4xl">Clef</h1>
+    </div>
+  );
+}
+
+export default App;
